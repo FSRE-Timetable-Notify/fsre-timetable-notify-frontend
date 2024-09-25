c6c445170e336a9a5257cce4617617b648882dfe
mapokapo <leopetrovic11@gmail.com>
2024-09-25 22:39:31 +0200

    Update API client base url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,3 +1,7 @@
 import { Api } from "./api";
 
-export const client = new Api();
+export const client = new Api({
+  baseUrl: import.meta.env.PROD
+    ? "http://204.216.216.141:5000"
+    : "http://localhost:5000",
+});
